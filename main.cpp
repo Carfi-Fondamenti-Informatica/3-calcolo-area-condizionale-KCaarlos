05272cab87df19b4a0ae7c67f8358035eac6db41
KCaarlos <115487880+KCaarlos@users.noreply.github.com>
2022-11-17 23:08:53 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,5 +2,25 @@
 using namespace std;
 
 int main() {
+    
+float a=0,b=0;
+
+cin>>a>>b;
+    
+int opzione=0;
+   
+cin>>opzione;
+    
+   switch (opzione) {
+         
+        case 0:cout<<((a*b)/2)<<endl;
+            break;
+        case 1:cout<<(a*a)<<endl;
+            break;
+        case 2:cout<<(a*b)<<endl;
+            break;
+        default:
+            cout<<"stringa non valida"<<endl;
+         
    return 0;
 }
